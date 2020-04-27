ae3c89a3207c79465e7079b8d944b7f5c1eeafaf
Andre Loreth <a.loreth@adito.de>
2020-04-27 09:44:07 +0200

    chore: apply codestyle on demo project

--- onoffswitch-demo/src/main/java/com/vaadin/addon/onoffswitch/demo/DemoUI.java
+++ onoffswitch-demo/src/main/java/com/vaadin/addon/onoffswitch/demo/DemoUI.java
@@ -1,9 +1,6 @@
 package com.vaadin.addon.onoffswitch.demo;
 
 import com.vaadin.addon.onoffswitch.OnOffSwitch;
-
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
@@ -14,37 +11,38 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+import javax.servlet.annotation.WebServlet;
+
 @Theme("demo")
 @Title("OnOffSwitch Add-on Demo")
 @SuppressWarnings("serial")
-public class DemoUI extends UI
-{
+public class DemoUI extends UI {
 
-    @WebServlet(value = "/*", asyncSupported = true)
-    @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class)
-    public static class Servlet extends VaadinServlet {
-    }
+  @WebServlet(value = "/*", asyncSupported = true)
+  @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class)
+  public static class Servlet extends VaadinServlet {
+  }
 
-    @Override
-    protected void init(VaadinRequest request) {
+  @Override
+  protected void init (VaadinRequest request) {
 
-    	final OnOffSwitch onOffSwitch = new OnOffSwitch(false);
-        onOffSwitch.addValueChangeListener(event -> {
-            boolean checked = event.getValue();
-            System.out.println("OnOffSwitch checked : " + checked);
-        });
+    final OnOffSwitch onOffSwitch = new OnOffSwitch(false);
+    onOffSwitch.addValueChangeListener(event -> {
+      boolean checked = event.getValue();
+      System.out.println("OnOffSwitch checked : " + checked);
+    });
 
-        onOffSwitch.addFocusListener(event -> System.out.println("FOCUS"));
+    onOffSwitch.addFocusListener(event -> System.out.println("FOCUS"));
 
-        Button toggle = new Button();
-        toggle.addClickListener(event -> onOffSwitch.setValue(!onOffSwitch.getValue()));
+    Button toggle = new Button();
+    toggle.addClickListener(event -> onOffSwitch.setValue(!onOffSwitch.getValue()));
 
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setSizeFull();
-        layout.addComponent(onOffSwitch);
-        layout.addComponent(toggle);
-        layout.setComponentAlignment(onOffSwitch, Alignment.MIDDLE_CENTER);
-        setContent(layout);
-    }
+    final VerticalLayout layout = new VerticalLayout();
+    layout.setSizeFull();
+    layout.addComponent(onOffSwitch);
+    layout.addComponent(toggle);
+    layout.setComponentAlignment(onOffSwitch, Alignment.MIDDLE_CENTER);
+    setContent(layout);
+  }
 
 }
